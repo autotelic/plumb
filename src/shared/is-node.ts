41f60aebc2c0ffa6add9758270ec5a3f6a49f37e
Ted Ogmundson <togmund@gmail.com>
2026-08-23 16:55:42 -0700

    Add missing JSDoc types to is-node

--- src/shared/is-node.ts
+++ src/shared/is-node.ts
@@ -5,8 +5,8 @@ import { isRecordObject, type NodeFieldValue, readField, isString } from "./stru
 /**
  * Whether a reflected node-field value is an AST node (as opposed to a child
  * array, literal, or absent).
- * @param value - Value obtained from reading an ESTree node field.
- * @returns True when the value is a single AST node.
+ * @param {NodeFieldValue} value - Value obtained from reading an ESTree node field.
+ * @returns {boolean} True when the value is a single AST node.
  */
 export function isNode(value: NodeFieldValue): value is ESTree.Node {
 	return isRecordObject(value) && isString(readField(value, "type"));
